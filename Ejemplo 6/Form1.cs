7d64662d11ea824dd254b9b07e8401b378662300
Ayla_SAP6 <Ayla_SAP6@DESKTOP-AJ19N0V>
2023-05-09 20:53:32 +0200

    Ejemplo 6 SqlConnection

--- Ejemplo 6/Form1.cs
+++ Ejemplo 6/Form1.cs
@@ -17,13 +17,55 @@ namespace Ejemplo_6
 
             using (SqlConnection conexion = new SqlConnection(cadenaConexion))
             {
+                SqlTransaction transaccion = null;
                 try
                 {
                     conexion.Open();
+
+                    transaccion = conexion.BeginTransaction();
+
+
+                    SqlCommand comando = conexion.CreateCommand();
+                    comando.CommandText = "SELECT Nombre,PoblacionId,Direccion,CP FROM Tabla_Centro WHERE Nombre LIKE @Nombre AND ID = @CentroId";
+                    
+                    SqlParameter parametro = new SqlParameter("@CentroId",SqlDbType.BigInt);
+                    parametro.Value = 2;
+                    comando.Parameters.Add(parametro);
+
+                    parametro =  new SqlParameter("@Nombre",SqlDbType.VarChar);
+                    parametro.Value = "%Guadalajara";
+                    comando.Parameters.Add(parametro);
+
+                    
+
+                    SqlDataReader reader = comando.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    var pipo = from x in dt.AsEnumerable()
+                               select x;
+
+                    /*
+                    long ID;
+                    int CP, PoblacionID;
+                    string  Nombre, Direccion;
+
+                    while (reader.Read())
+                    {
+                        ID = Convert.ToInt64(reader["Id"]);
+                        Nombre = reader["Nombre"].ToString();
+                        PoblacionID = Convert.ToInt32(reader["PoblacionId"]);
+                        Direccion = reader["Direccion"].ToString();
+                        CP = Convert.ToInt32(reader["CP"]);
+                    }
+                    */
+                    transaccion.Commit();
+
                 }
                 catch (Exception ex)
                 {
-
+                    transaccion.Rollback();
                 }
                 finally
                 {
