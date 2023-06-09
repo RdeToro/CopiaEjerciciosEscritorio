5740489eecf1c0ced8bded98d8a9881abd377c2d
Rdetoro95 <tdetoro95@gmail.com>
2023-06-10 01:55:53 +0200

    Formulario ICP desde 0

---
+++ Proyecto ICP desde 0/ConexionBBDD/ConexionBBDD.cs
@@ -0,0 +1,113 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ConexionBBDD
+{
+    public class ConexionBBDD
+    {
+        private string _CadenaConexion;
+        public SqlConnection Conexion { get; set; }
+        public SqlTransaction Transaccion { get; set; }
+
+        public SqlParameter Parametros { get; set; }
+
+
+        public bool Abrir ()
+        {
+            bool bResultado = false;
+            Conexion = new SqlConnection (_CadenaConexion);
+            Conexion.Open ();
+
+            if(Conexion.State == ConnectionState.Open )
+                bResultado = true;
+
+            return bResultado;
+
+        }
+
+        public bool Cerrar()
+        {
+            bool bResultado = false;
+            Conexion.Close ();
+
+            if(Conexion.State == ConnectionState.Closed )
+                bResultado = true;
+
+            return bResultado;
+        }
+
+        public DataTable Leer(string Sql)
+        {
+            DataTable resultado = null;
+            if(Conexion.State == ConnectionState .Open )
+            {
+                SqlCommand cmd = Conexion.CreateCommand ();
+                
+                cmd.CommandText = Sql;
+
+                SqlDataReader reader = cmd.ExecuteReader ();
+
+                resultado.Load(reader);
+
+                
+            }
+            return resultado;
+        
+        }
+
+        public int Escribir (string SQL, List<Parametro> parametros) 
+        {
+            SqlCommand comando = Conexion.CreateCommand ();
+            comando.CommandText = SQL;
+            
+            if(parametros != null && Conexion.State == ConnectionState.Open)
+            {
+
+                foreach(Parametro param in parametros)
+                {
+                    Parametros.ParameterName = String.Format("@" + param.Nombre);
+                    Parametros.DbType = param.Tipo;
+                    Parametros.Value = param.Valor;
+
+                    if(param.Direccion == Parametro.Direction.Entrada)
+                    {
+                        Parametros.Direction = ParameterDirection.Input;
+                    }
+                    else if (param.Direccion == Parametro.Direction.Salida)
+                    {
+                        Parametros.Direction = ParameterDirection.Output;
+                    }
+                    else if(param.Direccion == Parametro.Direction.EntradaSalida)
+                    {
+                        Parametros.Direction = ParameterDirection.InputOutput;
+                    }
+
+                    comando.Parameters.Add(param);
+                    
+                    comando.ExecuteNonQuery ();
+
+                    SQL = "Select @@Identity";
+
+
+                    comando = new SqlCommand(SQL);
+
+                    SqlDataReader reader = comando.ExecuteReader();
+
+                    DataTable dataTable = new DataTable();
+                    int resultado = 0;
+                    if(reader != null)
+                    {
+                        dataTable.Load(reader);
+                        resultado = Convert.ToInt32(dataTable.Rows[0][0]);
+                    }
+                    
+                    return resultado;
+                }
+
+                
+            }
+
+        }
+
+    }
+}m
---
+++ Proyecto ICP desde 0/Proyecto ICP desde 0/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_ICP_desde_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
