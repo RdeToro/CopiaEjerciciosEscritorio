64ed4164bb55ea0f7b1703cd4a78ace03a863434
Ayla_SAP6 <Ayla_SAP6@DESKTOP-AJ19N0V>
2023-06-21 16:21:37 +0200

    Formulario ICP

---
+++ Formulario ICP/FormularioICP sin petar/InsertarAvisoRecepcion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioICP_sin_petar
+{
+    public partial class Insertar_Aviso_Recepcion : Form
+    {
+        public Insertar_Aviso_Recepcion()
+        {
+            InitializeComponent();
+        }
+    }
+}
